c19ead943ccfc0cbcf3fb7544ad1008a9369cf3b
srinath ragalla <srinathragalla17@gmail.com>
2021-12-14 23:59:31 +0530

    logical oprts

---
+++ logical oprts.js
@@ -0,0 +1,25 @@
+if(20<10 && 4>2 && 20<20){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+
+if(20>10 && 4>2 && 10<20){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+
+if(100>200 || 200>300 || 400<500){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+if(10>20 || 20>30 || 50<30){
+    console.log(true)
+}else{
+    console.log(false)
+}
